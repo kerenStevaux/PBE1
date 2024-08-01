44933ffc6640174ae4f245f33c9e77fbb0968deb
Keren Stevaux <159180442+kerenStevaux@users.noreply.github.com>
2024-08-01 09:42:42 -0300

    Add files via upload

---
+++ programação_backend/atividade03.java
@@ -0,0 +1,27 @@
+package programcao_backend;
+
+import java.util.Scanner;
+
+public class atividade03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Qual é seu número? ");
+		int num = sc.nextInt();
+		
+		System.out.print("Qual é seu número de horas trabalhadas? ");
+		int horas = sc.nextInt();
+		
+		
+		System.out.print("Qual é o valor que você ganha por hora? ");
+		int valor = sc.nextInt();
+		
+		System.out.println("Funcionário " + (num) + " tem " + (horas) +
+				" horas trabalhadas, o valor de sua hora é R$" + (valor));
+		
+		sc.close();
+		
+	}
+}
