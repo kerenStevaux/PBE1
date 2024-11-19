eefeae0fdc6674fa258d01a92b6a8fd0f7c60fe6
Keren Stevaux <159180442+kerenStevaux@users.noreply.github.com>
2024-11-19 08:15:00 -0300

    Delete ATIVIDADE13/rhsenai-main/src directory

--- ATIVIDADE13/rhsenai-main/src/main/java/com/senaidev/rhsenai/repositories/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.rhsenai.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.rhsenai.entities.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
-
-}
