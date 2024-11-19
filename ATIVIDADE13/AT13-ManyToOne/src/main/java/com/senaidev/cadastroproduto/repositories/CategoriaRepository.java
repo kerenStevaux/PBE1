396874da44f436dca44d46969fe2d9cf012f868d
Keren Stevaux <159180442+kerenStevaux@users.noreply.github.com>
2024-11-19 12:54:48 -0300

    Delete ATIVIDADE13 directory

--- ATIVIDADE13/AT13-ManyToOne/src/main/java/com/senaidev/cadastroproduto/repositories/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.senaidev.cadastroproduto.repositories; 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.senaidev.cadastroproduto.entities.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> { }
