eab3d5e6bd06d11c7477bde9f0bb21ceff419035
Keren Stevaux <159180442+kerenStevaux@users.noreply.github.com>
2024-12-03 10:32:37 -0300

    Delete ATIVIDADE15/AT15-BancoLivraria/clienteEndereco directory

--- ATIVIDADE15/AT15-BancoLivraria/clienteEndereco/src/main/java/com/senaidev/clienteEndereco/repositories/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.clienteEndereco.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.clienteEndereco.entities.Endereco;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-
-}
